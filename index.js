2596c7adcf4637fb5c9eb81fa71ed8bcd31303b7
rahmat-81 <rahmat.fatih21@gmail.com>
2021-01-03 16:03:55 +0700

    test

--- index.js
+++ index.js
@@ -45,8 +45,10 @@ async function compare(monthResponse){
   const dayLast=parseInt(monthResponse.data[monthResponse.length-1].hijri.day);
   const month=monthResponse.data[0].hijri.month.number;
   const month2=parseInt(month);
-  const shaumMonth1= await fetch(`https://database-mstei-rahmat-test-202.herokuapp.com/api/${month2}`);
-  const shaumMonth2= await fetch(`https://database-mstei-rahmat-test-202.herokuapp.com/api/${month2+1}`);
+  const shaumMonth1= await fetch(`https://database-mstei-rahmat-test-202.herokuapp.com/api/${month2}`)
+  .then(response => {return response.json()});
+  const shaumMonth2= await fetch(`https://database-mstei-rahmat-test-202.herokuapp.com/api/${month2+1}`)
+  .then(response => {return response.json()});
   shaumMonth1.date.forEach(element => {
     for(let i=0;i++;i<monthResponse.length){
       if(monthResponse.data[i].hijri.month.number===month){
